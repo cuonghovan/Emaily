42801848b1d3ae01dc6f5e3ea6fcce044a477610
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-04-02 13:31:21 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send({ hi: "Cuong" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+// localhost:5000
+app.listen(PORT);
