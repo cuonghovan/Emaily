df03184c4d9ed1d08653781fc15cf5a954b9e938
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-04-04 16:01:28 +0700

    Config

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 passport.use(new GoogleStrategy());
-
-// Id: 191931269296-vc68uk44ftre4nc59qgprfiaaitn5gb9.apps.googleusercontent.com
-// Secret: M5fcPYkwglkIKdg1Wk_kCuui
-// heroku: https://lychee-tart-89195.herokuapp.com/
-
 const PORT = process.env.PORT || 5000;
+
+// heroku: https://lychee-tart-89195.herokuapp.com/
 app.listen(PORT);
