e1caea4802cb5308f10424544d0ab51ab74d9601
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-04-12 14:04:27 +0700

    Store given user info to DB and use cookie

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,18 @@
 const passport = require("passport");
+const mongoose = require("mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user));
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +22,19 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log('refresh tocken', refreshToken);
-      console.log('profile', profile);
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            // A record with given ID already existed
+            done(null, existingUser);
+          } else {
+            // A record with given ID is not existed, create new one
+            new User({
+              googleId: profile.id
+            }).save()
+              .then(user => done(null, user));
+          } 
+        });
     }
   )
 );
