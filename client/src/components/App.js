6775648635c1a708209e0240d7724715d3efd7ba
Cuong Ho <cuong.ict.10101@gmail.com>
2018-04-25 15:22:43 +0700

    Add materialize-css lib

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-
-const Header = () => <h2>Header</h2>;
+import Header from "./Header";
 const Landing = () => <h2>Landing</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const ServeyNew = () => <h2>ServeyNew</h2>;
@@ -9,7 +8,7 @@ const ServeyNew = () => <h2>ServeyNew</h2>;
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
+      <div className="container">
         <Header />
         <Route exact path="/" component={Landing} />
         <Route exact path="/serveys" component={Dashboard} />
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
+import "materialize-css/dist/css/materialize.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import rootReducers from "./reducers";
-
 import App from "./components/App";
 
 const store = createStore(rootReducers, {}, applyMiddleware());
