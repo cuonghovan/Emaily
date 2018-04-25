66255d59a9bac337b22290367af62ec527e32fc1
Cuong Ho <cuong.ict.10101@gmail.com>
2018-04-25 10:25:04 +0700

    Auth reducer

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import rootReducers from "./reducers";
 
 import App from "./components/App";
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(rootReducers, {}, applyMiddleware());
 
 ReactDOM.render(
   <Provider store={store}>
