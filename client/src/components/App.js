cd3a7fe3f085190bf109da0f304b7afcad797f1d
Cuong Ho <cuong.ict.10101@gmail.com>
2018-04-25 11:21:33 +0700

    Add router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-const App = () => <h1>App</h1>;
+const Header = () => <h2>Header</h2>;
+const Landing = () => <h2>Landing</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const ServeyNew = () => <h2>ServeyNew</h2>;
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/serveys" component={Dashboard} />
+        <Route path="/serveys/new" component={ServeyNew} />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
