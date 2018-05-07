e3a438cb11acd493be3e298392ffa6889b3a842d
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-05-07 10:50:43 +0700

    Check oauth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from 'react-redux';
 import Header from "./Header";
+import * as actions from '../actions';
 const Landing = () => <h2>Landing</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const ServeyNew = () => <h2>ServeyNew</h2>;
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="container">
-        <Header />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/serveys" component={Dashboard} />
-        <Route path="/serveys/new" component={ServeyNew} />
-      </div>
-    </BrowserRouter>
-  );
-};
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/serveys" component={Dashboard} />
+          <Route path="/serveys/new" component={ServeyNew} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from 'redux-thunk';
 import rootReducers from "./reducers";
 import App from "./components/App";
 
-const store = createStore(rootReducers, {}, applyMiddleware());
+const store = createStore(rootReducers, {}, applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
