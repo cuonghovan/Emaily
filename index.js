1cdc1bdc5651ab88a3ea0c743c26ff73760ecb35
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-05-11 16:13:18 +0700

    Routing in production

--- index.js
+++ index.js
@@ -23,8 +23,23 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Express will serve up in priority order
+// 1. Fisrt looking for request handlers.
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // 2. Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // 3. Express will serve up the index.html file
+  // if it does'nt recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
