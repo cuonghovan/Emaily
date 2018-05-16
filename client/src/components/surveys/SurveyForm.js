51e60dacc32e632c4ce068f59466981ece5c9879
Cuong Ho <cuong.ict.10101@gmail.com>
2018-05-16 14:24:17 +0700

    Custom field

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { reduxForm, Field, values } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+  {name: 'title', label: 'Survey Title'},
+  {name: 'subject', label: 'Subject Line'},
+  {name: 'body', label: 'Email Body'},
+  {name: 'emails', label: 'Recipient List'}
+];
 
 class SurveyForm extends React.Component {
+  renderFields() {
+    return (
+      <div>
+        {FIELDS.map(({name, label}) => <Field key={name} type='text' name={name} label={label} component={SurveyField} />)}
+      </div>
+    );
+  }
+  
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type='text' name='surveyTitle' component='input' />
-          <button type='submit'>Submit</button>
+          {this.renderFields()}
+          <Link to='/surveys' className='red btn-flat left white-text'>
+            Cancel
+          </Link>
+          <button type='submit' className='teal btn-flat right white-text'>
+            Next
+            <i className='material-icons right'>done</i>
+          </button>
         </form>
       </div>
     );
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SurveyForm from './SurveyForm';
 
 const SurveyNew = () => (
