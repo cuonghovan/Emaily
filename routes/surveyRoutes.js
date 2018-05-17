f5f3814bfdaa053e0cb8fe74ffeefd8bc66e93a7
Cuong Ho <cuong.ict.10101@gmail.com>
2018-05-17 14:17:05 +0700

    Fix local tunnel crash

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -35,4 +35,9 @@ module.exports = app => {
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thanks for voting!');
   });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body);
+    res.send({});
+  });
 };
